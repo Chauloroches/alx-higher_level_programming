ceecd3a8fa32e62f94b73f3b915166b58d481d43
Chauloroches <rocjeschaulo@gmail.com>
2024-03-22 15:53:10 -0700

    00-weight_average.py

---
+++ 0x04-python-more_data_structures/00-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    numbers = 0
+    i = 0
+
+    for x in my_list:
+        num += x[0] * x[1]
+        den += x[1]
+
+    return (numbers / i)
