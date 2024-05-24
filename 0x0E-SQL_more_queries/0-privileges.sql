9d7858f4c4a188104b55e5bd9450f4d4aeac5fc7
Chauloroches <rocjeschaulo@gmail.com>
2024-05-24 06:30:56 -0700

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,16 +1,8 @@
--- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost)
+-- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
 
--- Create the users 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Grant some privileges 
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-GRANT SELECT, INSERT, UPDATE ON *.* TO 'user_0d_2'@'localhost';
-
--- Flush privileges to update changes
-FLUSH PRIVILEGES;
-
--- List all privileges for user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_od_1'@'localhost';
+
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'user_0d_1'@'localhost';
+
+FLUSH PRIVILEGES;
