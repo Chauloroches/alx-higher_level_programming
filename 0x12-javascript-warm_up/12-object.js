fe12ed63b2a6836efc6e33af89e66fe6c4d1f1a0
Chauloroches <rocjeschaulo@gmail.com>
2024-06-17 05:08:23 -0700

    12-object.js

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* Update this script to replace the value 12 with 89 */
+const object = {
+  type: 'object',
+  value: 12
+};
+
+console.log(object);
+
+object.value = 89;
+
+console.log(object);
