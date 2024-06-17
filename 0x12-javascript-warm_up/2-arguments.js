0972cf3e36474cf8a0fbb706947cef10043fec9c
Chauloroches <rocjeschaulo@gmail.com>
2024-06-17 02:33:20 -0700

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 /* Script that prints a message depending of the number of arguments passed */
 const args = process.argv;
 
-if (args.length === 0) {
+if (args.length <= 0) {
   console.log('No argument');
-} else if (args.length === 1) {
+} else if (args.length === 2) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
