c7430e47b7b73ef9a076ccdb7c06d052c3338749
Chauloroches <rocjeschaulo@gmail.com>
2024-07-09 06:07:31 -0700

    1-hbtn_header.py

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Response header value #0
+
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    
+    with urllib.request.urlopen(url) as response:
+        headers = response.getheaders()
+        for header in headers:
+            if header[0] == "X-Request-Id":
+                print(header[1])
+                break
