4737da7e66dfa94b7f7d06dc1575a2b770619ca8
Chauloroches <rocjeschaulo@gmail.com>
2024-07-09 02:48:36 -0700

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
 # Function that finds a peak in a list of unsorted integers
 def find_peak(list_of_integers):
-    if not list_of_integers:
+    """Function to find the peak in a list of unsorted integers"""
+    if list_of_integers == []:
         return None
-
-    def find_peak_util(arr, low, high, n):
-        mid = (low + high) // 2
-
-        if (mid == 0 or arr[mid - 1] <= arr[mid]) and (mid == n - 1 or arr[mid + 1] <= arr[mid]):
-            return arr[mid]
-        elif mid > 0 and arr[mid - 1] > arr[mid]:
-            return find_peak_util(arr, low, mid - 1, n)
-        else:
-            return find_peak_util(arr, mid + 1, high, n)
-
-    return find_peak_util(list_of_integers, 0, len(list_of_integers) - 1, len(list_of_integers))
+    size = len(list_of_integers)
+    if size ==0:
+        return (None)
+    elif size == 0:
+        return (list_of_integers[0])
+    elif size == 1:
+        return max(list_of_integers)
+    mid = int(size/2)
+    peak = list_of_integers[mid]
+    list = list_of_integers
+    if peak > list[mid - 1] and peak > list[mid + 1]:
+        return peak
+    elif peak < list[mid -1]:
+        return find_peak(list[:mid])
+    else:
+        return find_peak(list[mid + 1:])
